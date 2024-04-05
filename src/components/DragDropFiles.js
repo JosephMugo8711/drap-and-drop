a084d38632d99c9343cea2345910f75802b0359a
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-05 23:36:07 +0300

    update and comments

--- src/components/DragDropFiles.js
+++ src/components/DragDropFiles.js
@@ -1,26 +1,39 @@
 import React, {useState, useRef} from 'react'
 
 
+// Functional component called DragDropFiles
 const DragDropFiles = () => {
-    // file state
+    // State to hold the selected files
     const [files, setFiles] = useState(null);
+    // Reference to the file input element
     const inputRef = useRef();
 
 
+    // Function to handle the drag over event
     const handleDragOver = (event) => {
+        // Prevent the default behavior of drag over
         event.preventDefault();
 
     }
 
+    //  Function to handle the drop event
     const handleDrop = (event) => {
+        //  Prevent the default behavior of drop
         event.preventDefault();
+        // Set the selected files from the dataTransfer object of the event
         setFiles(event.dataTransfer.files)
         // console.log(event);
 
     }
 
-    const handleUpload = () => {};
+    //  Function to handle the file upload
 
+    const handleUpload = () => {
+        // Placeholder function for handling the upload of files
+    };
+
+
+    //  If files are selected, render the list of files with upload and cancel buttons
     if (files) return (
         <div className='uploads'>
             <ul>
@@ -35,6 +48,7 @@ const DragDropFiles = () => {
         </div>
     )
 
+    //  If no files are selected, render the dropzone area and file selection button
   return (
     <>
     {/* Conditional rendering
