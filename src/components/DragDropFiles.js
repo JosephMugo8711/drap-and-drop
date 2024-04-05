3bdf6ee42e5b77c5130b3388dea1cb3cc0a7da6d
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-05 23:21:16 +0300

    draganddrop

--- src/components/DragDropFiles.js
+++ src/components/DragDropFiles.js
@@ -19,6 +19,22 @@ const DragDropFiles = () => {
 
     }
 
+    const handleUpload = () => {};
+
+    if (files) return (
+        <div className='uploads'>
+            <ul>
+                {Array.from(files).map((file, idx) => <li key={idx}>{file.name}</li>)}
+            </ul>
+            <div className='actions'>
+                <button onClick={() => setFiles(null)}>Cancel</button>
+                <button onClick={handleUpload}>Upload</button>
+
+            </div>
+
+        </div>
+    )
+
   return (
     <>
     {/* Conditional rendering
